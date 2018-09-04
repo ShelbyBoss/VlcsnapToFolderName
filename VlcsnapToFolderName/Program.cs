c71a26618da42c9b7ff3bdf7dc16abe6e64ec214
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-09-04 20:06:55 +0200

    Initial commit

---
+++ VlcsnapToFolderName/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrdnerDatei;
+
+namespace VlcsnapToFolderName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) return;
+
+            Ordner ordner = new Ordner(args[0], true);
+            Datei[] dateien = ordner.GetDateien();
+
+            if (dateien.Length == 0) return;
+
+            foreach(Datei datei in dateien)
+            {
+                string newPath = datei.GetDirectoryName() + "\\" + datei.GetFileName().Replace("vlcsnap-", ordner.Name + " ");
+                datei.MoveTo(newPath);
+            }
+        }
+    }
+}
